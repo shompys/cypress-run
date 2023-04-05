581f41a47b68c64c7c1081754688884a435d5241
Jonathan <jonathangomezit@gmail.com>
2023-04-05 01:21:16 -0300

    harcode url temporally

--- cypress/e2e/1-vite/press-button.cy.js
+++ cypress/e2e/1-vite/press-button.cy.js
@@ -1,6 +1,6 @@
 describe('main', () => {
     before(() => {
-        cy.visit('./')
+        cy.visit('http://localhost:8080/')
     })
     it('press the button 6 times and you must have the text with the quantity', () => {
         cy.get('button').dblclick().dblclick().dblclick().should('have.text', 'count is 6')
